a5bf7047ff64a44f0bfa3d4e073626eb769e7a82
jbriseno13 <104550107+jbriseno13@users.noreply.github.com>
2022-11-08 20:30:49 -0800

    Video List does not overflow, video plays onclick

--- client/src/components/VideoItem.js
+++ client/src/components/VideoItem.js
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 
-
-
 const VideoItem = ({ searchQuery }) => {
-  const [videos, setVideos] = useState({});
-  const { currVid, setCurrVid } = useState();
+  const [videos, setVideos] = useState([]);
+  const [ currVid, setCurrVid ] = useState("");
 
   const getVideos = async () => {
     const response = await fetch(`http://localhost:8091/search/${searchQuery}`);
     const videoItems = await response.json();
-    setVideos(videoItems);
+    setVideos(videoItems.items)
+    setCurrVid(videoItems.items[0].id.videoId)
   };
 
   useEffect(() => {
@@ -18,42 +17,54 @@ const VideoItem = ({ searchQuery }) => {
 
   return (
     <div>
-      Video Test {videos?.items?.[0]?.id?.videoId}
+      Video Test {videos?.id?.videoId}
       {/* //conditional rendering */}
-      {videos.items?.length > 0 }
+      {videos.length > 0 && (
+        <iframe
+          src={`https://www.youtube.com/embed/${currVid}`}
+          frameborder="0"
+          height="350px"
+          width="450px"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+        />
+     
+      ) }
       {/* iframe  goes here */}
       <div className="cards_wrapper">
-        {videos.items &&
-          videos.items.map((video, index) => {
+        {videos &&
+          videos.map((video, index) => {
             return (
-              
               <div className="video-cards" key={index}>
                 {video.id.videoId}
                 <div
-                  onClick={() => setCurrVid(video)}
+                  onClick={() => setCurrVid(video.id.videoId)}
                   className=" video-item item"
                 >
-                  <iframe src={`https://www.youtube.com/embed/${video.id.videoId}`}
-                  frameborder="0"
-                  height="350px"
-                  width="450px"
-                  allow="autoplay; encrypted-media"
-                  allowfullscreen
-                  title="video"
-                    /> 
-                   
-                  <div className="content">
-                    <div className="header ">{video.snippet.title}</div>
-                  </div>
-                </div> 
-                 {/* <div> <VideoDetail/></div> */}
+                  {/* <iframe
+                    src={`https://www.youtube.com/embed/${video.id.videoId}`}
+                    frameborder="0"
+                    height="350px"
+                    width="450px"
+                    allow="autoplay; encrypted-media"
+                    allowfullscreen
+                    title="video"
+                  /> */}
 
-                {/* <button
+                  <div className="ui segment">
+                    <h4 className="ui header">{video.snippet.title}</h4>
+                    <p>{video.snippet.description}</p>
+                  </div>
+                </div>
+                {/* <div> <VideoDetail/></div> */}
+
+                <button
                   className="play-next"
                   onClick={() => setCurrVid(video.id.videoId)}
                 >
                   Play
-                </button> */}
+                </button>
                 <button>Favorites</button>
               </div>
               //onClick setCurrVid to video.id.videoId
