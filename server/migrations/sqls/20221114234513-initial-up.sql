d7fb917e70f31855001384709338aedeeb332ad0
jbriseno13 <104550107+jbriseno13@users.noreply.github.com>
2022-11-21 22:53:35 -0800

    deleted migration data

--- server/migrations/sqls/20221114234513-initial-up.sql
+++ server/migrations/sqls/20221114234513-initial-up.sql
@@ -87,35 +87,6 @@ CREATE TABLE public.favvideos (
 );
 
 
---
--- Name: migrations; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.migrations (
-    id integer NOT NULL,
-    name character varying(255) NOT NULL,
-    run_on timestamp without time zone NOT NULL
-);
-
-
---
--- Name: migrations_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.migrations_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: migrations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.migrations_id_seq OWNED BY public.migrations.id;
 
 
 --
@@ -234,14 +205,6 @@ ALTER TABLE public.favvideos ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
     CACHE 1
 );
 
-
---
--- Name: migrations id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.migrations ALTER COLUMN id SET DEFAULT nextval('public.migrations_id_seq'::regclass);
-
-
 --
 -- Name: landingpagecontent appdescription_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
@@ -259,11 +222,6 @@ ALTER TABLE ONLY public.books
 
 
 --
--- Name: migrations migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.migrations
-    ADD CONSTRAINT migrations_pkey PRIMARY KEY (id);
 
 
 --
