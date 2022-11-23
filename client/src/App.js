d9ad8070669aaff2a5de9bdc8ecac482979cbfe9
jbriseno13 <104550107+jbriseno13@users.noreply.github.com>
2022-11-23 11:07:42 -0800

    not logged in user css fixed

--- client/src/App.js
+++ client/src/App.js
@@ -145,17 +145,33 @@ function App() {
 
             {/* <!-- Contact Section --> */}
             <div className="main" id="contactus">
-              <div className="main__container">
+             
                 <div className="main__img--container">
                   <div className="main__img--card">
+                    <fieldset className="contact-form-area">
+                      <form className="contact-form">
+                      <h1>Contact Us!</h1>
+                 
+                        <label>Name</label>
+                        <input className="form-name" type="text"></input>
+                        <br></br>
+                        <label>Email</label>
+                        <input className="form-email" type="email"></input>
+                        <br></br>
+                        <label>Message</label>
+                        <textarea className="form-message"></textarea>
+                        <br></br>
+                        <button className="form-submit">Submit</button>
+                      </form>
+                    </fieldset>
                     <i className="fas fa-layer-group"></i>
                   </div>
                 </div>
                 <div className="main__content">
-                  <h1>Contact Us!</h1>
-                  <h2>Contact Information!</h2>
+                  {/* <h1>Contact Us!</h1>
+                  <h2>Contact Information!</h2> */}
                 </div>
-              </div>
+            
             </div>
           </section>
 
@@ -172,7 +188,59 @@ function App() {
                 )} */}
               </div>
             </header>
-            <footer className="home-landing"></footer>
+            <footer className="home-landing">
+            <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/about/">About Us</a></li>
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+       
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+         <a href="#">Scanfcode</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+              
+            </footer>
           </div>
         </>
       )}
