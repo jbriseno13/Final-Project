9c397a92bf969b48f94fa80339268b4f5bb7bd26
jbriseno13 <104550107+jbriseno13@users.noreply.github.com>
2022-11-23 14:46:23 -0800

    drop down list added to form

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Form = () => {
   const { isAuthenticated, user } = useAuth0();
@@ -11,24 +11,24 @@ const Form = () => {
     question: "",
     post: "",
   });
- 
+
   const getPosts = async () => {
-   // const response = await fetch(`/api/posts`);
+    // const response = await fetch(`/api/posts`);
     const response = await fetch("/api/allposts", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({sub: user.sub}),
+      body: JSON.stringify({ sub: user.sub }),
     });
-    
+
     const posts = await response.json();
     console.log("Post Here", posts);
-   setPosts([...posts]);
+    setPosts([...posts]);
     setNewPost(posts[0].user_id);
   };
-  
+
   useEffect(() => {
     getPosts();
   }, []);
@@ -61,62 +61,73 @@ const Form = () => {
     });
   };
 
-
   return (
     <div className="post-section">
       <div className="post-wrapper">
-        {posts && posts.map((post, index) => { //will only run if posts is truthy- not an empty string, null, undefined. 
-          return (
-            <div className="posts-cards" key={index}>
-              <div className="posts-date">{post.date}</div>
-              <div className="posts-question">{post.question}</div>
-              <div className="posts-post">{post.post}</div>
-     
-              {/* <button
+        {posts &&
+          posts.map((post, index) => {
+            //will only run if posts is truthy- not an empty string, null, undefined.
+            return (
+              <div className="posts-cards" key={index}>
+                <div className="posts-date">{post.date}</div>
+                <div className="posts-question">{post.question}</div>
+                <div className="posts-post">{post.post}</div>
+
+                {/* <button
                     className="post-dlt-btn"
                     onClick={() => handleDeletePosts(posts.id)}
                   >
                     Delete
                   </button> */}
-    </div>
-          );
-        })}
+              </div>
+            );
+          })}
       </div>
-      <form className="day-note-section" onSubmit= {(e) => handleSubmit(e)}>
+      <form className="day-note-section" onSubmit={(e) => handleSubmit(e)}>
         <div id="form-card">
           <fieldset>
-          <div className="form-section">
-            <label>Today's Date</label>
-            <input
-              type="date"
-              id="add-date-of-sighting"
-              value={newPost.date}
-              onChange={(e) => {
-                //passing in event
-                setNewPost((prev) => ({
-                  ...prev, //whatever is prev in obj us spread op.. and them here
-                  date: e.target.value,
-                }));
-            }}
-            />
-            <br />
-            <label>How do you feel?</label>
-            
-    
-            <input
-              type="text"
-              id="fav-question"
-              value={newPost.question}
-              placeholder="Feeling"
-              required
-              onChange={(e) => {
-                //passing in event
-                setNewPost((prev) => ({
-                  ...prev, //whatever is prev in obj us spread op.. and them here
-                  question: e.target.value,
-                }));
-            }}
-            />
+            <div className="form-section">
+              <label>Today's Date</label>
+              <input
+                type="date"
+                id="add-date-of-sighting"
+                value={newPost.date}
+                onChange={(e) => {
+                  //passing in event
+                  setNewPost((prev) => ({
+                    ...prev, //whatever is prev in obj us spread op.. and them here
+                    date: e.target.value,
+                  }));
+                }}
+              />
+              <br />
+              <label>How do you feel?</label>
+
+              <select
+                type="text"
+                id="fav-question"
+                value={newPost.question}
+                // placeholder="Feeling"
+                required
+                onChange={(e) => {
+                  //passing in event
+                  setNewPost((prev) => ({
+                    ...prev, //whatever is prev in obj us spread op.. and them here
+                    question: e.target.value,
+                  }));
+                }}
+              >
+                <option value="Good">Good</option>
+                <option value="Sad">Sad</option>
+                <option value="Angry">Angry</option>
+                <option value="Anxious">Anxious</option>
+                <option value="Overwhelmed">Overwhelmed</option>
+                <option value="Calm">Calm</option>
+                <option value="Chill">Chill</option>
+                <option value="Joyful">Joyful</option>
+                <option value="Loved">Loved</option>
+                <option value="Appreciated">Appreciated</option>
+              </select>
             </div>
             <br />
             <label>Journal</label>
@@ -133,11 +144,11 @@ const Form = () => {
                   ...prev, //whatever is prev in obj us spread op.. and them here
                   post: e.target.value,
                 }));
-            }}
+              }}
             />
           </fieldset>
         </div>
-        <button className="add-form-btn" type="submit" >
+        <button className="add-form-btn" type="submit">
           Add
         </button>
       </form>
