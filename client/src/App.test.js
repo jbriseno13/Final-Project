d8d62aea69cf097fe9d7026bbde107de7bcee40f
jbriseno13 <104550107+jbriseno13@users.noreply.github.com>
2022-11-27 23:50:32 -0800

    created jest tests for components

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,42 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import VideosPage from './components/navBarPrivate/videospage'; 
+import  NavBar from './components/navbar'; 
+import VideoItem from "./components/VideoItem"; 
+import Favorite from "./components/favorites";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("VideosPage", () => {
+  test("renders videospage component", () => {
+    render(<VideosPage/>)
+  });
 });
+
+describe("NavBar", () => {
+  test("renders NavBar component", () => {
+    render(<NavBar/>)
+  });
+}); 
+
+
+describe("VideoItem", () => {
+  test("renders video item component", () => {
+    render(<VideoItem/>)
+  });
+}); 
+
+describe("Favorite", () => {
+  test("renders favorites components", () => {
+    render(<Favorite/>)
+  });
+});
+
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 
